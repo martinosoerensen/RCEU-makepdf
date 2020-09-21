e7514548203787441bcee28dfd72c628edcfcff6
Martin Sørensen <mos@laxity.dk>
2020-09-21 21:07:49 +0200

    fix line endings

--- build.cmd
+++ build.cmd
@@ -1,3 +1,4 @@
-docker volume remove makepdf_persist >nul
-docker build -t makepdf .
+docker volume remove makepdf_persist
+rem docker build -t makepdf .
+docker build -t martinosoerensen/rceu-makepdf https://github.com/martinosoerensen/RCEU-makepdf.git
 docker volume create --name makepdf_persist
